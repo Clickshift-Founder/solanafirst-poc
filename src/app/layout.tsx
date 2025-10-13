f99c8e2346ec9cb0b2823affbd41bf9e48293f9e
Emmanuel Ohanwe <clickndshift@gmail.com>
2025-10-13 14:40:43 +0100

    adding root layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+// src/app/layout.tsx
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "SolanaFirst | ClickShift",
+  description: "Proof of Concept for SolanaFirst integration",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
